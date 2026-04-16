992a90ab738016464ab898ac86acad67cb9d5049
vpliasetskyi <vpliasetskyi@gmail.com>
2026-04-15 22:03:11 -0700

    modify view.py

--- static_with_template/views.py
+++ static_with_template/views.py
@@ -10,4 +10,24 @@ def home(request):
 
 def contact(request):
     return render(request, 'static_with_template/contact.html')
+
+def p1(request):
+    body = {
+        'user': "John",
+        'login': True,
+        'password_length': 16
+    }
+    return render(request, 'static_with_template/page1.html', body)
+
+def p2(request):
+    list1 = {
+        'list1': [1, 2, 100, 200]
+    }
+    dict1 = {
+        'dict1': {"A": 1, "B": 2, "C": 3, "D": 4, "E": 3}
+    }
+    return render(request, 'static_with_template/page2.html', list1, dict1)
+
+
 # Create your views here.
+
